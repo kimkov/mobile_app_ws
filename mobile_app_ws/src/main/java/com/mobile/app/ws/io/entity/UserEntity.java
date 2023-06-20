a6d6e39ac27852b75cf373e2306d4a96985e997e
Oleg Kim <97583952+kimkov@users.noreply.github.com>
2023-06-20 09:02:24 +0600

    added some classes

--- mobile_app_ws/src/main/java/com/mobile/app/ws/io/entity/UserEntity.java
+++ mobile_app_ws/src/main/java/com/mobile/app/ws/io/entity/UserEntity.java
@@ -1,13 +1,18 @@
 package com.mobile.app.ws.io.entity;
 
 import java.io.Serializable;
+import java.util.Collection;
 import java.util.List;
 
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.OneToMany;
 
 @Entity(name="users")
@@ -41,6 +46,12 @@ public class UserEntity implements Serializable{
 	
 	@OneToMany(mappedBy="userDetails", cascade=CascadeType.ALL)
 	private List<AddressEntity> addresses;
+	
+	@ManyToMany(cascade = {CascadeType.PERSIST}, fetch = FetchType.EAGER)
+	@JoinTable(name = "users_roles",
+			joinColumns = @JoinColumn(name = "users_id", referencedColumnName = "id"),
+			inverseJoinColumns = @JoinColumn(name = "roles_id", referencedColumnName = "id"))
+	private Collection<RoleEntity> roles;
 
 	public long getId() {
 		return id;
